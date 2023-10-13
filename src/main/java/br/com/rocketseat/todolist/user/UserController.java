7a5fa88ff32c1ac62d61e9e417ae57ead0175f1e
dor-95 <diego.rocha@edu.unipar.br>
2023-10-13 19:41:50 -0300

    feat: created UserController

---
+++ src/main/java/br/com/rocketseat/todolist/user/UserController.java
@@ -0,0 +1,34 @@
+package br.com.rocketseat.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    IUserRepository userRepository;
+
+    @Autowired
+    public UserController(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+}
