fbfb638147f1f7015017ae2fb6e6f6161a195d72
dor-95 <diego.rocha@edu.unipar.br>
2023-10-14 17:47:52 -0300

    feat: added date validations

--- src/main/java/br/com/rocketseat/todolist/task/TaskController.java
+++ src/main/java/br/com/rocketseat/todolist/task/TaskController.java
@@ -1,10 +1,17 @@
 package br.com.rocketseat.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -18,9 +25,28 @@ public class TaskController {
     }
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
-        var task = this.taskRepository.save(taskModel);
-        return task;
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+
+        LocalDateTime currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("A data de início deve ser maior do que a data atual");
+        }
+
+        if (currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("A data de término deve ser maior do que a data atual");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("A data de início deve ser menor do que a data de término");
+        }
+
+        TaskModel taskCreated = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
     }
 
 
