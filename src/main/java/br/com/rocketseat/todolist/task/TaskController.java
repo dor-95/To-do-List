c5077743c42cf7347b98869a10c80fa9c4c06fa5
dor-95 <diego.rocha@edu.unipar.br>
2023-10-15 02:36:48 -0300

    added merging of non-null properties in update

--- src/main/java/br/com/rocketseat/todolist/task/TaskController.java
+++ src/main/java/br/com/rocketseat/todolist/task/TaskController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.rocketseat.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -60,12 +61,13 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-        Object idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id) {
+        
+        TaskModel task = this.taskRepository.findById(id).orElse(null);
 
-        TaskModel taskUpdated = this.taskRepository.save(taskModel);
+        Utils.copyNonNullProperties(taskModel, task);
+
+        TaskModel taskUpdated = this.taskRepository.save(task);
         return ResponseEntity.status(HttpStatus.OK).body(taskUpdated);
     }
 }
